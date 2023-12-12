dab2aeac740b7f887232d11b5e550fa1ca9e3db9
Ema Blanco <emablanco@disroot.com>
2023-12-12 15:20:34 -0300

    alarma

--- alarma/ESP32/ESP32.ino
+++ alarma/ESP32/ESP32.ino
@@ -1,26 +1,48 @@
-
-int buzzer =4;
-int frecuencia = 500;
+#include <Arduino.h>
+int buzzer = 4;
+int frecuencia = 200;
 int canal = 0;
 
-void setup() {
-  ledcSetup(canal, frecuencia, 8);
-  ledcAttachPin(buzzer, canal);
+int sensor = 2;
+unsigned long tiempo;
+bool peligro = false;
+
+bool movimiento();
+void alarma(const int &);
+
+void setup(){
+  pinMode(sensor,INPUT);
+  ledcSetup(canal,frecuencia,8);
+  ledcAttachPin(buzzer,canal);
+  Serial.begin(115200); 
 }
 
-void loop() {
-  for(frecuencia; frecuencia < 2000; frecuencia+= 15){
-    tono(frecuencia,20); 
+void loop(){
+  tiempo = millis();
+    if(movimiento()){
+      Serial.println("Estado alto");
+      while(millis() - tiempo < 10000){
+        for(frecuencia ; frecuencia < 2000; frecuencia +=10){
+          alarma(10);
+        }
+        for(frecuencia ; frecuencia > 500; frecuencia -=10){
+          alarma(10);
+        }
+      }
+      peligro = !peligro;
+    }
   }
-  for(frecuencia ; frecuencia > 500; frecuencia -= 15){
-    tono(frecuencia, 20);
+
+bool movimiento(){
+  if(digitalRead(sensor) == HIGH){
+    peligro = !peligro;
   }
+  return peligro;
 }
 
-void tono(int f, int tiempo){
-  ledcWriteTone(canal,f);
-  ledcWrite(canal,146);
-  delay(tiempo);
-  ledcWrite(canal,0);
-  //delay(tiempo);
+void alarma(const int &t){
+  ledcWriteTone(canal,frecuencia);
+  ledcWrite(canal,128);
+  delay(t);
+  ledcWrite(canal, 0);
 }
